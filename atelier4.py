d676aa8f162566801dcac9eb470bce4c4eb1178b
zizoubouchelkia <zizoubouchelkia9@gmail.com>
2026-03-09 20:08:57 -0400

    Ajout des methodes de la classe Employe

--- atelier4.py
+++ atelier4.py
@@ -4,3 +4,37 @@ class Employe:
         self.nom = nom
         self.prenom = prenom
         self.voiture_service = None
+
+    def afficher_informations(self):
+        print("Employe :")
+        print("Numero de permis :", self.numero_permis)
+        print("Nom :", self.nom)
+        print("Prenom :", self.prenom)
+
+        if self.voiture_service is not None:
+            print("Voiture de service attribuee :")
+            print("  Matricule :", self.voiture_service.matricule)
+            print("  Annee :", self.voiture_service.annee)
+            print("  Marque :", self.voiture_service.marque)
+            print("  Kilometrage :", self.voiture_service.kilometrage)
+        else:
+            print("Aucune voiture de service attribuee.")
+        print("-" * 40)
+
+    def affecter_voiture(self, voiture):
+        if self.voiture_service is not None:
+            print(self.prenom, self.nom, "a deja une voiture.")
+        elif voiture.chauffeur is not None:
+            print("Cette voiture est deja attribuee a un autre employe.")
+        else:
+            self.voiture_service = voiture
+            voiture.chauffeur = self
+            print("Voiture attribuee a", self.prenom, self.nom)
+
+    def retirer_voiture(self):
+        if self.voiture_service is None:
+            print(self.prenom, self.nom, "n'a aucune voiture a retirer.")
+        else:
+            self.voiture_service.chauffeur = None
+            self.voiture_service = None
+            print("Voiture retiree de", self.prenom, self.nom)
