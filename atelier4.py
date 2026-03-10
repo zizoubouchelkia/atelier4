25c8b2ccabcd32bebe1e02b123a4bb9f30368df5
zizoubouchelkia <zizoubouchelkia9@gmail.com>
2026-03-09 20:02:20 -0400

    Creation de la classe Employe avec ses attributs

---
+++ atelier4.py
@@ -0,0 +1,6 @@
+class Employe:
+    def __init__(self, numero_permis, nom, prenom):
+        self.numero_permis = numero_permis
+        self.nom = nom
+        self.prenom = prenom
+        self.voiture_service = None
